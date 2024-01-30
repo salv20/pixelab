1f6e7eebc6fe13e80df5652388774eae30cc5a6c
Salvation Amoke <salvationamoke@gmail.com>
2024-01-30 07:17:16 +0100

    padding

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Testimonial from "./components/testimonial"
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-6 xl:px-20 xxl:px-44 container mx-auto overflow-hidden relative">
+    <div className="px-4 xl:px-10 container mx-auto overflow-hidden relative">
       <Header />
       <Hero />
       <Service/>
